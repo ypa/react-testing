23519bc2d0ecc57b64f04209b049c95a7e5ee30e
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-28 20:24:17 -0800

    Add CommentBox and CommentList components with boiler plate

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import CommentBox from './CommentBox';
+import CommentList from './CommentList';
+
 
 export default () => {
   return (
     <div>
-      Im the app component
+      <CommentBox />
+      <CommentList />
     </div>
   );
 }
