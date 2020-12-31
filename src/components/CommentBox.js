1c3ca886743189635db24528d9c8399c46c0b095
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-30 20:23:02 -0800

    Make the textarea field a controlled input with react component level state

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
+  state = { comment: '' };
+
+  handleChange = (event) => {
+    this.setState({ comment: event.target.value });
+  };
+
   render() {
     return (
       <form>
         <h4>Add a Comment</h4>
-        <textarea />
+        <textarea onChange={this.handleChange} value={this.state.comment} />
         <div>
           <button>Submit Comment</button>
         </div>
