5e16a696f33efa3f4a341c8660c36edb30f059ab
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-02 20:35:55 -0800

    Refactor text area tests with describe block and own beforeEach

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,24 +17,23 @@ it('has a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in ', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment' }
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    wrapped.update();
   });
 
-  wrapped.update();
-
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-});
-
-it('clears text area when the form is submitted', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'my comment' }
+  it('has a text area that users can type in ', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
   });
-  wrapped.update();
-  wrapped.find('form').simulate('submit');
 
-  wrapped.update();
+  it('clears text area when the form is submitted', () => {
+    wrapped.find('form').simulate('submit');
 
-  expect(wrapped.find('textarea').prop('value')).toEqual('');
-});
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
+})
