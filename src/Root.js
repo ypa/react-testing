edafb692cf8454c777adb99c45c03ff63b50fdd1
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-03 20:12:58 -0800

    Wire up Redux store via Root component so that both components inside tests and actual code can be wrapped with and be happy

---
+++ src/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from 'reducers';
+import { checkPropTypes } from 'prop-types';
+
+export default (props) => {
+  return (
+    <Provider store={createStore(reducers, {})}>
+      {props.children}
+    </Provider>
+  );
+}
--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
   state = { comment: '' };
@@ -10,10 +12,9 @@ class CommentBox extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
 
-    // TODO - Call a Redux action creator
-    // and save the comment
+    this.props.saveComment(this.state.comment);
 
-    this.setState({comment: ''});
+    this.setState({ comment: '' });
   };
 
   render() {
@@ -29,4 +30,4 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
