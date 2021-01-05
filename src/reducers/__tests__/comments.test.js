c0d343a728b8bcbe543f370e8569215b95f6b2fa
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-04 20:00:52 -0800

    Add comments reducer test that handles an unknown action type

--- src/reducers/__tests__/comments.test.js
+++ src/reducers/__tests__/comments.test.js
@@ -9,4 +9,11 @@ it('handles actions of type SAVE_COMMENT', () => {
 
   const newState = commentsReducer([], action);
   expect(newState).toEqual(['New Comment']);
-})
+});
+
+
+it('handles action with unknown type', () => {
+  const newState = commentsReducer([], { type: 'adfasdf' });
+
+  expect(newState).toEqual([]);
+});
