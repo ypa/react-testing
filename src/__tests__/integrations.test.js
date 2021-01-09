45108557cb33a9029bd7db17a91213e4536bd85b
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-08 20:43:17 -0800

    Add fetch comments integration with moxios to stub out the axios request call

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios';
 import Root from 'Root';
 import App from 'components/App';
 
+beforeEach(() => {
+  // Intercept all axios requrests
+  moxios.install();
+  // Stub out all the requests
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Fetch #1' }, { name: 'Fetch #2' }]
+  });
+});
 
-it('can fetch a list of comments and display them', () => {
+afterEach(() => {
+  moxios.uninstall();
+});
+
+
+it('can fetch a list of comments and display them', (done) => {
   // Attempt to render the *entire* app
   const wrapped = mount(
     <Root>
@@ -13,8 +28,18 @@ it('can fetch a list of comments and display them', () => {
   );
 
   // find the 'fetchComments' button and click it
+  wrapped.find('.fetch-comments').simulate('click');
 
-  // Expect to find a list of comments!
+  // Need to wait a little 100ms before we can verify
+  setTimeout(() => {
+    wrapped.update();
+
+    // Expect to find a list of comments!
+    expect(wrapped.find('li').length).toEqual(2);
+
+    done();
+    wrapped.unmount();
+  }, 100);
 });
 
 
