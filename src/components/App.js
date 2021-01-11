84631946ddf4ab27b82b9dbf3a1c8769fdad0097
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-10 20:20:19 -0800

    Refactor App component into class based component to make room for a couple of helper methods that will get added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
 
-export default () => {
-  return (
-    <div>
-      <Route path="/post" component={CommentBox} />
-      <Route path="/" exact component={CommentList} />
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
 }
