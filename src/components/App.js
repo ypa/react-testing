97cf36db6faf83e28973fc147ac7fc5aa274da2e
Yan P Aung <yan.pye.aung@gmail.com>
2021-01-10 20:29:26 -0800

    Add navigation headers

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
 
-export default class App extends Component {
+class App extends Component {
+  renderButton() {
+    if (this.props.auth) {
+      return (
+        <button>Sign Out</button>
+      );
+    } else {
+      return (
+        <button>Sign In</button>
+      );
+    }
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post a Comment</Link>
+        </li>
+        <li>
+          {this.renderButton()}
+        </li>
+      </ul>
+    )
+  }
+
   render() {
     return (
       <div>
+        {this.renderHeader()}
         <Route path="/post" component={CommentBox} />
         <Route path="/" exact component={CommentList} />
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(App);
